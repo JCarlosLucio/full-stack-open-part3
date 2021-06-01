c4f0c456831ddbb266992059ccde3914eca9e52d
jcLucio <c.lucio3@gmail.com>
2021-06-01 10:09:55 -0500

    Add phonebook backend exercise (3.5)

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     name: 'Arto Hellas',
@@ -50,6 +52,23 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end();
   }
+});
+
+const generateId = () => {
+  return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+};
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = [...persons, person];
+  response.json(person);
 });
 
 app.delete('/api/persons/:id', (request, response) => {
