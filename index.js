92a64c3a5b0c3193ae253b40b2524d7c83faff83
jcLucio <c.lucio3@gmail.com>
2021-06-03 12:29:15 -0500

    Add phonebook database exercise (3.19)

--- index.js
+++ index.js
@@ -54,12 +54,6 @@ app.get('/api/persons/:id', (request, response, next) => {
 app.post('/api/persons', (request, response, next) => {
   const { name, number } = request.body;
 
-  if (!name || !number) {
-    return response.status(400).json({
-      error: 'name or number missing',
-    });
-  }
-
   const person = new Person({
     name,
     number,
@@ -107,6 +101,8 @@ const errorHandler = (error, request, response, next) => {
   console.log(error.message);
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
   next(error);
 };
